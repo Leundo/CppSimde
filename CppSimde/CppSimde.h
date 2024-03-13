bdeb38f5e83a961401848efd8a69d42104c0c833
Leundo <daysundoing@outlook.com>
2024-03-13 23:41:22 +0800

    Initial Commit

---
+++ CppSimde/CppSimde.h
@@ -0,0 +1,18 @@
+//
+//  CppSimde.h
+//  CppSimde
+//
+//  Created by Undo Hatsune on 2024/03/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CppSimde.
+FOUNDATION_EXPORT double CppSimdeVersionNumber;
+
+//! Project version string for CppSimde.
+FOUNDATION_EXPORT const unsigned char CppSimdeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CppSimde/PublicHeader.h>
+
+
